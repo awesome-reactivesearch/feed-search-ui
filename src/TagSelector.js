969ecfbd24b5f468d1206f92026d895c1f2a9b18
Shubham Shah <shubhamshahrising@gmail.com>
2023-02-28 20:07:04 +0530

    feat: more tags

--- src/TagSelector.js
+++ src/TagSelector.js
@@ -33,6 +33,12 @@ export default function TagSelector({ options, setOptions }) {
           <option value="Family">Family</option>
           <option value="Documentary">Documentary</option>
           <option value="History">History</option>
+          <option value="Crime">Crime</option>
+          <option value="Drama">Drama</option>
+          <option value="Mystery">Mystery</option>
+          <option value="Romance">Romance</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Action">Action</option>
         </select>
       </div>
     </div>
